8c12df994693bc1b0340b37ee94fdde8b5325f4b
Xize Wu <wuxize1995@gmail.com>
2023-04-18 19:43:13 -0400

    update class name for day list item

---
+++ src/components/DayListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from "classnames";
+import "./DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, setDay, spots, selected } = props;
+  const listClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+
+  return (
+    <li className = {listClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      {spots > 1 && (<h3 className="text--light">{spots} spots remaining</h3>)}
+      {spots === 1 && (<h3 className="text--light">{spots} spot remaining</h3>)}
+      {spots === 0 && (<h3 className="text--light">no spots remaining</h3>)}
+    </li>
+  );
+}
